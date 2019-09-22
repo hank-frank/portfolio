30ac006af4cde3ca004a3cc2c61771abeb69649a
hankfrank <henryfrank1@yahoo.com>
2019-09-22 10:00:59 -0700

    a

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -98,7 +98,7 @@ class App extends Component {
                                                return <img src={image} className="each-image"/>
                                             })
                                         }
-                                    </div>
+                                        </div>
                                     </div>
                                 </div>
                     })
