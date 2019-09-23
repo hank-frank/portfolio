52cdbf52f0ea745442eb237efdd8b01af30671ce
hankfrank <henryfrank1@yahoo.com>
2019-09-23 13:44:36 -0700

    adds formatting, linkedin email close icons

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import Carousel from './carousel.jsx'
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaEnvelopeSquare } from 'react-icons/fa';
+import { FaWindowClose } from 'react-icons/fa';
 
 // let images = require('../assets/site-images/CCC-1.png')
 // let images1 = require('../assets/site-images/CCC-2.png')
@@ -20,7 +19,6 @@ class App extends Component {
         this.visibilityWatcher = this.visibilityWatcher.bind(this);
         this.makeVisible = this.makeVisible.bind(this);
     }
-
 
     makeVisible (eachKey) {
         let projects = this.state.projectsArray
@@ -52,13 +50,18 @@ class App extends Component {
         } else if (status === true) {
             return "block"
         }
-
     }
-
 
     render () {
         return (
             <>
+                <div className="contact-container">
+                    <div className="each-contact"><a href="https://www.linkedin.com" target="_blank" title="LinkedIn" className="linkedin" ><FaLinkedinIn /></a></div>
+                    <div className="each-contact"><a href="https://www.linkedin.com" target="_blank" title="email" className="email" ><FaEnvelopeSquare /></a></div>
+                    <div className="each-contact"></div>
+                    <div className="each-contact"></div>
+                    <div className="each-contact"></div>
+                </div>
                 <div className="flex-container-vertical">
                     <img src={require("../assets/me-face.png")} className="portrait-img"/>
                     <h1 className="name-text">Hello, I'm <span id="henry">Henry</span>. I'm a Fullstack Developer living in San Diego.</h1>
@@ -67,7 +70,7 @@ class App extends Component {
                 <div className="flex-container-row">
                 {
                     this.state.projectsArray.map((each) => {
-                   return   <div key={ each.key }>
+                   return   <div key={ each.key } >
                                 <div className="card" onClick={() => this.makeVisible(each.key)}>
                                     <div className="card-circle">
                                         <p className="title">{each.title}</p>
@@ -77,14 +80,13 @@ class App extends Component {
                     })
                 }
                 </div>
-                <div>
                 {
                     this.state.projectsArray.map((each) => {
-                    return      <div key={ each.key } >
-                                    <div className="project-visibility-container" style={{ display: this.visibilityWatcher(each.isVisible)}}>
+                    return         <div className="project-visibility-container" key={ each.key } style={{ display: this.visibilityWatcher(each.isVisible)}}>
+                                    <FaWindowClose className="x" onClick={() => this.hideVisibility(each.key)}/>
                                     <div className="project-info-container">
                                         <div className="project-title-container">
-                                            <h4 className="project-title" onClick={() => this.hideVisibility(each.key)}>{ each.title }</h4>
+                                            <h4 className="project-title" >{ each.title }</h4>
                                             <p className="project-description">{ each.description }</p>
                                             <div className="link-flex">
                                                 <a className="link-button">Github</a>
@@ -100,10 +102,8 @@ class App extends Component {
                                         }
                                         </div>
                                     </div>
-                                </div>
                     })
                 }
-                </div>
             </>
         );
     }
